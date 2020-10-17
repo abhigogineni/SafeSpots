1eea5b848cb18033a6ff4040d51e174a669e5db4
Shreyas <schicker@purdue.edu>
2020-10-17 14:31:10 -0700

    adding good button functionality

--- app/src/main/java/com/example/safespots/MainActivity.java
+++ app/src/main/java/com/example/safespots/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button isQuarantined = (Button)(findViewById(R.id.isQuarantined));
+        Button isNotQuarantined = (Button)(findViewById(R.id.isNotQuarantined));
+        TextView textViewQuarantined = (TextView) findViewById(R.id.text_is_Quarantined);
+        EditText textViewName = (EditText) findViewById(R.id.nameInput);
+        TextView textViewTitle = (TextView) findViewById(R.id.Title_main);
+
+        textViewName.setText("pee pee poo poo check");
+
     }
-    Button medicalProblem = new Button(findViewById(R.id.medicalProblem);
-    Button symptom = new Button(findViewById(R.id.symptom);
-    TextView tv =  (TextView) findViewById(R.id.helloUser);
 
 }
---
+++ app/src/main/java/com/example/safespots/MenuActivity.java
@@ -0,0 +1,21 @@
+package com.example.safespots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView titleMenu = (TextView) findViewById(R.id.Title_menu);
+        TextView hello = (TextView) findViewById(R.id.hello);
+
+    }
+
+
+}
