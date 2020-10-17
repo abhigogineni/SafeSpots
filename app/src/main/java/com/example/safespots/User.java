5d4eba57b9c7ea041390975474155d8d35c13eb1
Shreyas <schicker@purdue.edu>
2020-10-17 10:37:44 -0700

    added User class with fields

---
+++ app/src/main/java/com/example/safespots/User.java
@@ -0,0 +1,8 @@
+package com.example.safespots;
+
+public class User {
+    private String name;
+    private int year; //1 for freshman 4 for senior, etc
+    private int[] requests = new int[4]; // 0 - medical help, 1 - food/water, 2 - Zoom/Company, 3 - Custom
+
+}
