a3dd413e67fcdb3857c23822680516808feb14e7
Shreyas <schicker@purdue.edu>
2020-10-17 14:45:50 -0700

    moved Buttons and TextViews into onCreate method

--- app/src/main/java/com/example/safespots/MainActivity.java
+++ app/src/main/java/com/example/safespots/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.safespots;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,15 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button isQuarantined = (Button)(findViewById(R.id.isQuarantined));
         Button isNotQuarantined = (Button)(findViewById(R.id.isNotQuarantined));
         TextView textViewQuarantined = (TextView) findViewById(R.id.text_is_Quarantined);
         EditText textViewName = (EditText) findViewById(R.id.nameInput);
         TextView textViewTitle = (TextView) findViewById(R.id.Title_main);
-
-        textViewName.setText("pee pee poo poo check");
-
     }
+
 
 }
